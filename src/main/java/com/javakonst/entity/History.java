111d98a6a01be364a9e1e74e4c2af3626818bdcc
Konstantin <kskiselev@mail.ru>
2020-08-27 07:48:49 +0500

    backup 27.08.20 07:48

--- src/main/java/com/javakonst/entity/History.java
+++ src/main/java/com/javakonst/entity/History.java
@@ -2,16 +2,31 @@ package com.javakonst.entity;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Data
+@javax.persistence.Entity
+@Table(name = "history")
 public class History extends Entity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    @Column
     private Date tradedate;
+    
+    @Column
     private Double numtrades;
+    
+    @Column
     private Double open;
+    
+    @Column
     private Double close;
-
+    
     @Override
     public String toString() {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
--- src/main/java/com/javakonst/entity/Security.java
+++ src/main/java/com/javakonst/entity/Security.java
@@ -2,9 +2,25 @@ package com.javakonst.entity;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
-public class Security extends Entity{
+@javax.persistence.Entity
+@Table(name = "securities")
+public class Security extends Entity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    @Column
     private String name;
+    
+    @Column
     private String emitent_title;
+    
+    @Column
     private String regnumber;
+    
+    //TODO: добавить связанный объект History
 }
