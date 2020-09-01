9e3ab19a3bb1b500ad524c51b94ba11d76e9281a
Konstantin <kskiselev@mail.ru>
2020-09-01 07:21:50 +0500

    backup 01.09.20 07:20

--- src/main/java/com/javakonst/ServiceAPI.java
+++ src/main/java/com/javakonst/ServiceAPI.java
@@ -2,6 +2,8 @@ package com.javakonst;
 
 import com.javakonst.dao.HistoryDAO;
 import com.javakonst.dao.SecuritiesDAO;
+import com.javakonst.db.DBService;
+import com.javakonst.db.DBServiceDAO;
 import com.javakonst.entity.History;
 import com.javakonst.entity.Security;
 import com.javakonst.utils.ListUtils;
@@ -11,23 +13,105 @@ import java.util.List;
 public class ServiceAPI {
     private List<Security> listSecuritires;
     private List<History> listHistory;
-
+    
     public ServiceAPI(String filepath_securities, String filepath_history) {
+        super();
         ListUtils listUtils = new ListUtils();
         listSecuritires = listUtils.getListSecuritires(filepath_securities);
         listHistory = listUtils.getListHistory(filepath_history);
     }
-
+    
+    public List<Security> listAllSecuritires() {
+        return listSecuritires;
+    }
+    
+    public Security listReadSecurityBySecid(String secid) {
+        SecuritiesDAO securitiesDAO = new SecuritiesDAO();
+        return securitiesDAO.entityRead(secid, listSecuritires);
+    }
+    
     public List<Security> listCreateSecurity(Security security) {
         SecuritiesDAO securitiesDAO = new SecuritiesDAO();
         securitiesDAO.entityCreate(security, listSecuritires);
         return listSecuritires;
     }
-
+    
+    public List<Security> listUpdateSecurity(Security security) {
+        SecuritiesDAO securitiesDAO = new SecuritiesDAO();
+        securitiesDAO.entityUpdate(security, listSecuritires);
+        return listSecuritires;
+    }
+    
+    public List<Security> listDeleteSecurityBySecid(String secid) {
+        SecuritiesDAO securitiesDAO = new SecuritiesDAO();
+        securitiesDAO.entityDelete(secid, listSecuritires);
+        return listSecuritires;
+    }
+    
+    public List<History> listAllHistory() {
+        return listHistory;
+    }
+    
+    public History listReadHistoryBySecid(String secid) {
+        HistoryDAO historyDAO = new HistoryDAO();
+        return historyDAO.entityRead(secid, listHistory);
+    }
+    
     public List<History> listCreateHistory(History history) {
         HistoryDAO historyDAO = new HistoryDAO();
         historyDAO.entityCreate(history, listHistory);
         return listHistory;
     }
-
+    
+    public List<History> listUpdateHistory(History history) {
+        HistoryDAO historyDAO = new HistoryDAO();
+        historyDAO.entityUpdate(history, listHistory);
+        return listHistory;
+    }
+    
+    public List<History> listDeleteHistoryBySecid(String secid) {
+        HistoryDAO historyDAO = new HistoryDAO();
+        historyDAO.entityDelete(secid, listHistory);
+        return listHistory;
+    }
+    
+    public void dbCreateDB() {
+        DBService dbService = new DBServiceDAO();
+        dbService.saveListsToDB(listSecuritires, listHistory);
+    }
+    
+    public String dbSaveSecurity(Security security) {
+        DBService dbService = new DBServiceDAO();
+        return dbService.saveSecurity(security);
+    }
+    
+    public List<Security> dbAllSecurity() {
+        DBService dbService = new DBServiceDAO();
+        return dbService.getAllSecurities();
+    }
+    
+    public Security dbSecurityBySecid(String secid) {
+        DBService dbService = new DBServiceDAO();
+        return dbService.getSecurityBySecid(secid);
+    }
+    
+    public int dbDeleteSecurityBySecid(String secid) {
+        DBService dbService = new DBServiceDAO();
+        return dbService.deleteSecurityBySecid(secid);
+    }
+    
+    public int dbDeleteHistoryBySecuritySecid(String secid) {
+        DBService dbService = new DBServiceDAO();
+        return dbService.deleteHistoryBySecuritySecid(secid);
+    }
+    
+    public void dbUpdateSecurity(Security security) {
+        DBService dbService = new DBServiceDAO();
+        dbService.updateSecurity(security);
+    }
+    
+    public void dbUpdateHistory(History history) {
+        DBService dbService = new DBServiceDAO();
+        dbService.updateHistory(history);
+    }
 }
